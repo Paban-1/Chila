a2ee5e233d246eabe89cd213ca7eea2c976773f0
Paban-1 <rpaban89@gmail.com>
2025-09-26 20:01:59 +0530

    added initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {FaStar} from 'react-icons/fa';
+function App() {
+ 
+
+  return (
+    <>
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <FaStar/>
+    </h1>
+    </>
+  )
+}
+
+export default App
